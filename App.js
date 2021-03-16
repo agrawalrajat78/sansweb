24ea735e2495be7f91bbf8d9150db73c10bc288e
agrawalrajat78 <71402319+agrawalrajat78@users.noreply.github.com>
2021-03-16 13:01:11 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,174 @@
+import React from 'react'
+
+
+const App = () => {
+  return (
+    <>
+
+      <div className="container-fluid row_design ">  <div className="container">
+        <nav className="navbar navbar-expand-lg navbar-light mx-auto">
+          <a className="navbar-brand  text-light mt-3" href="#"><p className="font_stlye">Sans Website Solutions</p></a>
+          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav ml-auto">
+              <li className="nav-item active">
+                <a className="nav-link  text-light" href="#"><p className="font_stlye_1">Home</p> <span className="sr-only">(current)</span></a>
+              </li>
+              <li className="nav-item ">
+                <a className="nav-link  text-light " href="#"><p className=
+                'font_stlye_1'>Contact</p></a>
+              </li>
+
+            </ul>
+
+          </div>
+        </nav>
+      </div></div>
+
+
+
+      <div className="container">
+        <div className="row">
+          <div  className="col-md-3 col-sm-12">
+            <div className="card card_design mt-5" >
+
+              <div className="card-body">
+                <p className="text-center font_size"><i class="fas fa-mobile-alt"></i></p>
+                <p className="card-text text-center text_size text-light">Mobile Apps</p>
+
+              </div>
+            </div>
+          </div>
+          <div className="col-md-3 col-sm-12">
+            <div className="card card_design mt-5">
+
+              <div className="card-body">
+                <p className="text-center font_size"><i class="fas fa-tv"></i></p>
+                <p className="card-text text-center text_size text-light">Web Application</p>
+
+              </div>
+            </div>
+          </div>
+          <div className="col-md-3 col-sm-12">
+            <div className="card card_design mt-5">
+
+              <div className="card-body">
+                <p className="text-center font_size"><i class="far fa-object-group"></i></p>
+                <p className="card-text text-center text_size text-light">Website Design</p>
+
+              </div>
+            </div>
+          </div>
+          <div className="col-md-3 col-sm-12">
+            <div className="card card_design mt-5">
+
+              <div className="card-body">
+                <p className="text-center font_size"><i class="fas fa-adjust"></i></p>
+                <p className="card-text text-center text_size text-light">SEO</p>
+
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="container mt-5">
+        <div className="row  g-2 d-flex justify-content-between">
+          <div className="sws_1">
+            <div className="row">
+
+              <div className="col-md-12">
+                <div className='row row_design_1 '>
+                  <div className="col-md-5 m-auto text-light ml-1">
+                    <p className="Sws_design">SWS</p>
+                    <p className="text-muted Sws_design">Spinning Web Solutions That Last</p>
+                  </div>
+                  <div className="col-md-4"></div>
+                  <div className="col-md-3 m-auto">
+                    <button className="btn butt">Contact Us</button>
+                  </div>
+                </div>
+              </div>
+              <div className="col-md-12 row_design_2 mt-4">
+                <button className="btn butt_1">Experties</button>
+                <p className="text-light Experties mt-3 mb-3">Web development is a mix of myriad jobs, from apps to websites, static to dynamic pages, server-side to client-side, architecture to data, content to e-commerce, and so much more. For a web solutions company to assist their client throughout their journey, experts in a number of areas are a necessity.
+                It is with that intention that we have assembled a team of web wizards well-versed in a slew of services like ReactJS, React Native, Javascript, HTML, CSS, Node JS, DotNet, MySQL, MongoDB, WordPress, Drupal, and more. With our squad of webmasters, you can rest assured that all your tech needs would be catered to and managed without any snags or setbacks in your business.</p>
+              </div>
+
+            </div>
+          </div>
+          <div className="sws_2 row_design_2  ">
+            <h4 className="text-light m-4 text-center"> Looking_for?</h4>
+            <ul className="text-light ml-5">
+              <li><a href="#">React Js</a></li>
+              <li><a href="#">React Native</a></li>
+              <li><a href="#">Java Script </a></li>
+              <li><a href="#">HTML & CSS</a></li>
+              <li><a href="#">Node Js</a></li>
+              <li><a href="#">Dot Net</a></li>
+              <li><a href="#">MySQL</a></li>
+              <li><a href="#">MongoDB</a></li>
+              <li><a href="#">Wordpress</a></li>
+              <li><a href="#">Drupal</a></li>
+
+
+
+
+
+            </ul>
+          </div>
+
+        </div>
+      </div>
+      <div className="container">
+        <div className="row m-1 mt-3">
+          <div className="col-md-12 row_design_2 Experties">
+            <button className="btn butt_1">Key</button>
+            <p className="mt-3">Setting up a website that is intuitive, user-friendly, responsive, and accurately represents your brand
+            Developing a web application to make it easier for people to interact with your business
+            Creating mobile applications and sites for your customers to access your products and services from their smartphones
+            Taking care of the SEO needs of your online presence to rank high and bring in more leads
+            Maintaining your website and apps so that they keep running smoothly and do not lose visitors due to downtime</p>
+          </div>
+
+        </div>
+      </div>
+      <div className="container">
+        <div className="row m-1 mt-3 ">
+          <div className="col-md-12 row_design_2 Experties">
+            <button className="btn butt_1">About Us</button>
+            <p className="mt-3 "><strong>There has been a meteoric rise in digitization across the globe,</strong></p>
+            <p>especially in the last few years. Naturally, the demand for quality web services has also increased. And that is exactly what Sans Web Solutions delivers. Our services range from something as simple as a static website to more complex web and mobile development.
+            We are ardent believers of versatility; therefore, we have developed a diverse skillset to be able to fulfil any common             web development project that comes our way. We have built up a multiskilled team consisting of experts in ReactJS,            Angular, React-Native, Python, NodeJS, MySQL, MongoDB… all the technologies commonly used for developing web solutions.           We ensure that the products we deliver are scalable, viable, and resilient so that no matter what your market is, you           will be ruling it in no time.</p>
+          </div>
+
+        </div>
+      </div>
+      <div className="container-fluid row_design_3 mt-5">
+        <div className="container"><div className="row">
+          <div className="col-md-6 text-light"><h3 className="my-5">Sans Web Solutions</h3>
+          <p>© 2021, All Rights Reserved.</p></div>
+          <div className="col-md-6 text-light text-right my-5"><p><strong>Address & Contact</strong></p>
+          <div><p>Address: 306, Princes' Business Skyline,</p></div>
+          <div><p>Vijay Nagar, Indore M.P. India (452001)</p></div>
+          <div><p>Mobile: +91-9584860344</p></div>
+          <div><p>Email: hr@sanswebsolutions.com</p></div>
+          
+         
+          
+          
+          
+          </div>
+        </div></div>
+      </div>
+
+    </>
+  )
+}
+
+export default App;
+
+
+
